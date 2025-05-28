983fe5934307460432a536161719f599d1845bca
Nagendra-web <nagendra.l@s.amity.edu>
2025-05-28 13:16:16 -0700

    update the player profile

--- gamee.py
+++ gamee.py
@@ -14,7 +14,7 @@ def check_winner(board, player):
         [board[0][0], board[1][0], board[2][0]],
         [board[0][1], board[1][1], board[2][1]],
         [board[0][2], board[1][2], board[2][2]],
-        [board[0][0], board[1][1], board[2][2]],
+        [board[0][0], board[3][4], board[2][2]],
         [board[0][2], board[1][1], board[2][0]],
     ]
     return [player, player, player] in win_conditions
